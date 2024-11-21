f4ee0d3fc4e7cec038a4d7fb5fd7c98ba79694f0
Raphael DE ALMEIDA <raphael.dealmeida@acensi.fr>
2024-11-21 16:10:42 +0100

    first commit

---
+++ back/server.js
@@ -0,0 +1,21 @@
+const Fastify = require('fastify');
+const fastifyCORS = require('@fastify/cors');
+
+const server = Fastify();
+
+server.register(fastifyCORS, {
+  origin: '*',
+  credentials: true,
+});
+
+server.get('/', async (request, reply) => {
+  return { message: 'Hello from Fastify!' };
+});
+
+server.listen({port: 3030, host:'0.0.0.0'}, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Server listening at ${address}`);
+});
